ed9c62a0100697fdea16f6197cc6375c842242be
Heldereim <adam.hellgard@hotmail.se>
2024-05-15 11:27:51 +0200

    Added some documentation to the flask app api

--- flask_app/query_data.py
+++ flask_app/query_data.py
@@ -16,11 +16,15 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/<country>", methods=['GET'])
-def query_data(country): # Returns a dataframe containing the country matching the input string and the data of that country.
-                  # Will return an empty dataframe (but containing its columns) if the input does not match a country.
-    #country = request.args.get('country',type=str)
+def query_data(country):
+    """
+    Returns a dataframe containing the country matching the input string and the data of that country.
+    Will return an empty dataframe (but containing its columns) if the input does not match a country
+    
+    Parameters:
+        - country: string | The name of the country
+    """
     dataset = pd.read_csv('flask_app/clumped_data.csv')
-    #values = dataset[dataset['Country'].str.contains(fr'{country}', case=False)].reset_index().to_json(orient='records')
     formatted = (dataset[dataset['Country'].str.match(fr'{country}', case=False)].reset_index()).dropna(axis=1, how='all').transpose()
     chosen_info = formatted.iloc[1: , :]
     html_table = chosen_info.to_html(classes='table',index_names=False, header=False, bold_rows=False, justify= 'center')
@@ -28,13 +32,21 @@ def query_data(country): # Returns a dataframe containing the country matching t
 
 @app.route("/chart/<country>", methods=['GET','POST'])
 def get_plot(country):
+    """
+    Generates a pie chart containing the given country's energy production by source.
+    If the given country already has a pie chart a new one will not be generated.
+    The response from the api comes in the form of an image html tag containing the source for the
+    generated image.
+
+    Parameters:
+        - country: string | The name of the country
+    """
     content = PROD_TEMPLATE
     content = content.replace('country_graph', country)
     if not(os.path.exists(f'sveltekit_app/static/country_prod/{country}.png')):
         dataset = pd.read_csv('flask_app/clumped_data.csv')
         energy_doc = (dataset[dataset['Country'].str.match(fr'{country}', case=False)])
         energy_doc = energy_doc.drop(['Code (alpha-3)', 'Country', 'Renewable energy production (%)'], axis=1).dropna(axis=1, how='all')
-        labels = energy_doc.columns.tolist()
         values = energy_doc.to_numpy().flatten()
         if (values.any()):
             fig,ax = plt.subplots()
@@ -46,13 +58,21 @@ def get_plot(country):
 
 @app.route("/consumption/<country>", methods=['GET','POST'])
 def get_con(country):
+    """
+    Generates a pie chart containing the energy consumption by source for the given country.
+    If the given country already has a pie chart a new chart will not be generated.
+    The response from the api comes in the form of an image html tag containing the source for the
+    generated image.
+
+    Parameters:
+        - country: string | The name of the country
+    """
     content = CON_TEMPLATE
     content = content.replace('country_graph', country)
     if not(os.path.exists(f'sveltekit_app/static/country_con/{country}.png')):
         dataset = pd.read_csv('flask_app/energyCon.csv')
         energy_doc = (dataset[dataset['Entity'].str.match(fr'{country}', case=False)])
         energy_doc = energy_doc.drop(['Code', 'Entity', 'Year'], axis=1).dropna(axis=1, how='all')
-        labels = energy_doc.columns.tolist()
         values = energy_doc.to_numpy().flatten()
         if (values.any()):
             fig,ax = plt.subplots()
