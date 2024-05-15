1ea85ad431c841c127f0bf68a5fcc27cae5fb9fc
Heldereim <adam.hellgard@hotmail.se>
2024-05-15 10:49:44 +0200

    Updated country consumption legend

--- flask_app/renewables.py
+++ flask_app/renewables.py
@@ -3,17 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import regex as re
 
-dataset = pd.read_csv('flask_app/clumped_data.csv')
-energy_doc = (dataset[dataset['Country'].str.match(fr'sweden', case=False)])
-energy_doc = energy_doc.drop(['Code (alpha-3)', 'Country', 'Renewable energy production (%)'], axis=1).dropna(axis=1, how='all')
+dataset = pd.read_csv('flask_app/energyCon.csv')
+energy_doc = (dataset[dataset['Entity'].str.match(fr'Sweden', case=False)])
+energy_doc = energy_doc.drop(['Code', 'Entity', 'Year'], axis=1).dropna(axis=1, how='all')
 new_cols = []
 for item in energy_doc.columns:
-    new_cols.append(item.replace('(GWh)', ''))
+    new_cols.append(item.replace(' - TWh', ''))
 energy_doc.columns = new_cols
 labels = energy_doc.columns.tolist()
 values = energy_doc.to_numpy().flatten()
 
-fig = plt.figure(figsize=(3,2.2))
+fig = plt.figure(figsize=(3,2.1))
 plt.rc('axes.spines', **{'bottom':False, 'left':False, 'right':False, 'top':False})
 ax = fig.add_subplot(111)
 wedges = [] 
@@ -23,4 +23,4 @@ legend = fig.legend(labels, loc='center')
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 plt.show()
-fig.savefig('ProdLegend.png')
+fig.savefig('ConLegend.png')
