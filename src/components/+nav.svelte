e8c4c536ec4f05a9b770e49df9f8536d6b37ee1d
kalimooo <kallewiklund2002@gmail.com>
2024-05-15 11:17:55 +0200

    finished test 1

--- src/components/+nav.svelte
+++ src/components/+nav.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import { handleFormSubmit, countriesTest } from "../scripts/mapInteractions";
     import { countryStore } from '../store/mapStore';
+
     let svgElement : SVGSVGElement;
+    let searchBar: HTMLInputElement | null;
 
     let selectedCountry : string = $countryStore;
     let autocompleteSuggestions: Set<string> = new Set<string>;
@@ -9,16 +11,20 @@
 
     function updateCountry(event: Event) {
         const input = event.target as HTMLInputElement;
-        countryStore.set(input.value);
+        selectedCountry = input.value;
+        countryStore.set(selectedCountry);
 
         autocompleteSuggestions = countriesTest($countryStore);
         showDropdown = autocompleteSuggestions.size > 0 && selectedCountry.length > 0;
     }
 
-    function selectSuggestion(suggestion: string) {
-        $countryStore = suggestion;
-        console.log("in selectSuggestion, " + $countryStore);
-        showDropdown = false; // Hide dropdown after selection
+    // Updates store variable and search bar text
+    function updateSearch(country: string) {
+        $countryStore = country;
+
+        if (searchBar != null) {
+            searchBar.value = country;
+        }
     }
 
 
@@ -43,12 +49,12 @@
     </div>
     <div class="flex justify-center items-center m-0 text-white text-4xl font-dosis">
         <form class="relative w-full flex max-h-full" on:submit="{handleFormSubmit}">
-            <input type="search" bind:value={selectedCountry} on:input="{updateCountry}" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none font-dosis" placeholder="Search countries, continents..." required />
+            <input type="search" bind:this={searchBar} bind:value={selectedCountry} on:input="{updateCountry}" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none font-dosis" placeholder="Search countries, continents..." required />
             <button type="submit" class="absolute border-2 right-2 top-2 text-white bg-[#323638] hover:opacity-[75%] focus:ring-4 focus:outline-none font-dosis rounded-lg text-sm px-4 py-2 ">Search</button>
             {#if showDropdown}
                 <ul class="absolute top-11 z-10 bg-white w-full mt-2 rounded-lg border border-gray-300">
                     {#each autocompleteSuggestions as suggestion}
-                    <button class="block w-full text-left px-4 py-2 focus:outline-none hover:bg-gray-100 text-black text-4xl font-dosis border-t border-b" on:click={handleFormSubmit} on:click={showDropdown=false}>
+                    <button class="block w-full text-left px-4 py-2 focus:outline-none hover:bg-gray-100 text-black text-4xl font-dosis border-t border-b" on:click={() => updateSearch(suggestion)}  on:click={handleFormSubmit} on:click={() => showDropdown=false}>
                         {@html boldMatch(suggestion, selectedCountry)}
                     </button>
                     {/each}
