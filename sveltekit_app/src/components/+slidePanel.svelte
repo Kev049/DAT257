a76b451cabf90fa4d3803018307e5b751514aead
Simon Hammerlid <simon.hammerlid@gmail.com>
2024-05-15 12:19:35 +0200

    trying to fix when charts are showed

--- sveltekit_app/src/components/+slidePanel.svelte
+++ sveltekit_app/src/components/+slidePanel.svelte
@@ -2,13 +2,16 @@
     import { countryContentStore, countryGraphStore, countryConStore } from '../store/mapStore';
     import { onMount } from 'svelte'
     import { currentImage, currentTable, currentCon } from '../scripts/mapInteractions';
-    import { packSiblings } from 'd3';
+    import { html, packSiblings } from 'd3';
+
+    let button: HTMLButtonElement;
 
     onMount(() => {
         countryContent = currentTable; 
         countryGraph = currentImage;
         countryCon = currentCon;
     });
+    
 
     $: countryContent = '';
     $: countryGraph = '';
@@ -30,6 +33,29 @@
 
     function toggleGraphs() {
         showImage = !showImage;
+
+        //console.log(table instanceof HTMLTableElement)
+        // console.log(countryGraph)
+    }
+
+    function shouldShowPieChart(){
+        var tempObject = document.createElement('div');
+        tempObject.innerHTML = countryContent;
+        var table = tempObject.querySelector('table');
+        var rowLength = table?.rows.length;
+        if( rowLength != undefined){
+            if(rowLength <= 3){
+                return false;
+            }
+        }
+    }
+
+    function updateButtonState() {
+        if (shouldShowPieChart()) {
+            button.classList.remove('opacity-50', 'cursor-not-allowed');
+        } else {
+            button.classList.add('opacity-50', 'cursor-not-allowed');
+        }
     }
 </script>
 
@@ -52,6 +78,7 @@
         <img src="country_con/ConLegend.png" alt="Legend for consumption graphs">
         {@html countryCon}
     {/if}
+    <!-- {#if !countryGraph} -->
     <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center w-1/4" on:click={toggleGraphs} id=toggleCharts>
         <img id="toggleIcon" src="../../pie-chart.png" alt="Icon"/>
     </button>
