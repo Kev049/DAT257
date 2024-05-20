40cf289914f3a01afc31791b54b3af17374deb2f
Simon Hammerlid <simon.hammerlid@gmail.com>
2024-05-20 17:57:05 +0200

    Buttons are greyed out now

--- sveltekit_app/src/components/+slidePanel.svelte
+++ sveltekit_app/src/components/+slidePanel.svelte
@@ -6,6 +6,7 @@
     import { get } from 'svelte/store';
 
     // let button: HTMLButtonElement;
+    let tooltipInfo: string = 'No data available';
 
     onMount(() => {
         countryContent = currentTable; 
@@ -39,7 +40,12 @@
 
     async function updateProdButton() {
         prodButtonDisabled = !shouldShowProd();
+        // document.getElementById("toggleProdChart")?.style.setProperty('-webkit-filter', 'grayscale(100%)');
+        // console.log(document.querySelector("spDiv"));
+        console.log(document.querySelectorAll("button"));
+
     }
+            // filter: grayscale(100%);
 
     async function updateConButton() {
         conButtonDisabled = !shouldShowConLegend();
@@ -121,6 +127,14 @@
         height: auto;
         max-height: 100%;
     }
+
+    .toggleChart:disabled {
+        color: #666;
+        background-color: #bdc3c7;
+        -webkit-filter: grayscale(100%);
+        filter: grayscale(100%);
+    }
+
 </style>
 
 <div class="absolute top-0 right-0 bottom-0 w-1/4 flex flex-col px-6 bg-white text-gray-700 z-10 border-l-4 border-[#333333]">
@@ -128,10 +142,10 @@
         <button style:background-color={tableButtonColor} class="py-2 px-4 rounded inline-flex button-tab;" on:click={showTable} id=toggleTable>
             <img id="toggleIcon" src="../../stats.png" alt="Icon"/>
         </button>
-        <button style:background-color={prodButtonColor} class="py-2 px-4 rounded inline-flex button-tab" on:click={showProd} disabled={prodButtonDisabled} id=toggleProdChart>
+        <button style:background-color={prodButtonColor} class="toggleChart py-2 px-4 rounded inline-flex button-tab" on:click={showProd} disabled={prodButtonDisabled} id=toggleProdChart>
             <img id="toggleIcon" src="../../wind-turbine.png" alt="Icon"/>
         </button>
-        <button style:background-color={conButtonColor} class="py-2 px-4 rounded inline-flex button-tab" on:click={showCon} disabled={conButtonDisabled} id=toggleConsChart>
+        <button style:background-color={conButtonColor} class="toggleChart py-2 px-4 rounded inline-flex button-tab" on:click={showCon} disabled={conButtonDisabled} id=toggleConsChart>
             <img id="toggleIcon" src="../../fire-place.png" alt="Icon"/>
             <!-- disabled ? 'disabled' : '' -->
         </button>
